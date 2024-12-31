2711af819673cebe71d3d02d97b2800d67237ca4
Ozsumit <oniwagon>
2024-12-31 21:26:25 +0545

    tyr

--- components/display/SearchDisplay.js
+++ components/display/SearchDisplay.js
@@ -9,8 +9,9 @@ const SearchDisplay = ({ movies }) => {
       ? movies.filter(
           (movie) =>
             movie &&
-            (movie.releaseDate || movie.release_date) &&
-            (movie.ratings || movie.vote_average !== undefined)
+            (movie.releaseDate ||
+              movie.poster_path ||
+              movie.release_date !== undefined)
         )
       : [];
 
