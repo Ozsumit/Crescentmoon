22d14b60fa33c302fe2f789b8f185892ed18035e
Ozsumit <oniwagon>
2025-01-15 13:05:25 +0545

    Refactor VIDEO_SOURCES in MovieInfo component to remove duplicate VidSrc entry and adjust download support

--- components/info/MovieInfo.js
+++ components/info/MovieInfo.js
@@ -38,6 +38,13 @@ const VIDEO_SOURCES = [
       "?primaryColor=63b8bc&secondaryColor=a2a2a2&iconColor=eefdec&icons=default&player=default&title=true&poster=true&autoplay=true&nextbutton=true&mute=false",
     icon: <Play className="w-4 h-4" />,
     downloadSupport: false,
+  }, {
+    name: "VidSrc",
+    params: "?multiLang=true",
+    url: `https://v2.vidsrc.me/embed/movie/`,
+    icon: <Award className="w-4 h-4" />,
+    downloadSupport: false,
+    // getDownloadLink: (id) => `https://v2.vidsrc.me/download/${id}`,
   },
   {
     name: "2Embed",
@@ -52,14 +59,7 @@ const VIDEO_SOURCES = [
     downloadSupport: false,
     parseUrl: true,
   },
-  {
-    name: "VidSrc",
-    params: "?multiLang=true",
-    url: `https://v2.vidsrc.me/embed/movie/`,
-    icon: <Award className="w-4 h-4" />,
-    downloadSupport: true,
-    getDownloadLink: (id) => `https://v2.vidsrc.me/download/${id}`,
-  },
+ 
   {
     name: "EmbedSu",
     url: `https://embed.su/embed/movie/`,
