44e319c31ec9e96554ea5e6229e88323cd6a05a4
Ozsumit <oniwagon>
2025-01-17 21:52:48 +0545

    Add SpeedInsights component and update package dependencies

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Footer from "@/components/footer/Footer";
 import AdBlocker from "@/components/filters";
 import WelcomeModal, { WelcomeModalTrigger } from "@/components/welcome";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,14 +15,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en"><meta name="msvalidate.01" content="C94A436E9262EFD0C59B769DBCBF17F7" />
+    <html lang="en">
+      <meta name="msvalidate.01" content="C94A436E9262EFD0C59B769DBCBF17F7" />
       <body className={inter.className}>
         <Header />
         <Analytics />
+        <SpeedInsights />
         <div className="mt-0">{children}</div>
         {/* <WelcomeModal onClose={() => {}} /> */}
         {/* <WelcomeModalTrigger>Open Welcome Guide</WelcomeModalTrigger>// Auto-show on first visit or version update
-
+<SpeedInsights/>
 // Or use the trigger button
 <WelcomeModalTrigger>Open Welcome Guide</WelcomeModalTrigger> */}
         <AdBlocker />
