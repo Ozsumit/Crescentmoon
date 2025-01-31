4124c7384f571ecfb57da597d5504bd099efdcea
ozsumit <pokhrelsumit36@gmail.com>
2025-01-31 12:47:14 +0545

    Initialize PostHog analytics in layout and ensure it runs only in production environments

--- app/layout.js
+++ app/layout.js
@@ -7,18 +7,7 @@ import WelcomeModal, { WelcomeModalTrigger } from "@/components/welcome";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { PostHogProvider } from "./providers";
-import posthog from "posthog-js";
-posthog.init("phc_b1sOYJSoCDWzjkxPxND3RNJoG7527n2WfVIkJHTpL9W", {
-  api_host: "https://us.i.posthog.com",
-});
-if (
-  !window.location.host.includes("127.0.0.1") &&
-  !window.location.host.includes("localhost")
-) {
-  posthog.init("phc_b1sOYJSoCDWzjkxPxND3RNJoG7527n2WfVIkJHTpL9W", {
-    api_host: "https://us.i.posthog.com",
-  });
-}
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
