cfdd750ae86b37bbd42e01c387acd48b02821ff2
ozsumit <pokhrelsumit36@gmail.com>
2025-02-14 17:00:09 +0545

    wdsq

--- next.config.js
+++ next.config.js
@@ -3,14 +3,8 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org", "imgur.com", "arc.io", "i.imgur.com"],
   },
-  publicRuntimeConfig: {
-    API_KEY: process.env.API_KEY, // Exposed to both server and client
-  },
-  serverRuntimeConfig: {
-    SERVER_API_KEY: process.env.SERVER_API_KEY, // Accessible only on the server
-  },
-  experimental: {
-    runtime: "edge", // Enables Edge runtime support
+  env: {
+    API_KEY: process.env.API_KEY,
   },
 };
 
