f04638371e6042a199c2b09500525861d2216cfa
Ozsumit <129023326+Ozsumit@users.noreply.github.com>
2025-04-18 12:01:14 +0545

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,8 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="msvalidate.01" content="C94A436E9262EFD0C59B769DBCBF17F7" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="ef34f090-3fa5-4f40-a41e-eb05a3710d1b"></script>
-    <script data-goatcounter="https://sumit.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+   <script data-goatcounter="https://sumit.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
 <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "de022bcad822493286b101c58245c2b9"}'></script>
     <script defer data-domain="cmoon.sumit.info.np" src="https://plausible.io/js/script.tagged-events.js"></script>
       </head>
