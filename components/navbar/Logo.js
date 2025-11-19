60b68027de21686bf8acd90cfd408c17c628948a
ozsumit <pokhrelsumit36@gmail.com>
2025-11-19 16:03:21 +0545

    ds

--- components/navbar/Logo.js
+++ components/navbar/Logo.js
@@ -1,8 +1,30 @@
 import Link from "next/link";
 import React from "react";
-import { Metal_Mania } from "next/font/google";
+import {
+  Metal_Mania,
+  Mountains_of_Christmas,
+  Festive,
+  Henny_Penny,
+} from "next/font/google";
 
 const metalMania = Metal_Mania({
+  weight: "400",
+  subsets: ["latin"],
+  preload: true,
+});
+const henny_penny = Henny_Penny({
+  weight: "400",
+  subsets: ["latin"],
+  preload: true,
+});
+
+const mountainsOfChristmas = Mountains_of_Christmas({
+  weight: "700",
+  subsets: ["latin"],
+  preload: true,
+});
+
+const festive = Festive({
   weight: "400",
   subsets: ["latin"],
   preload: true,
@@ -13,13 +35,18 @@ const Logo = () => {
     <Link
       href="/"
       className="inline-flex items-center text-2xl sm:text-4xl md:text-[3rem] font-bold tracking-tight transition-colors duration-200 hover:opacity-80"
-      aria-label="Crescent Moon Home"
+      aria-label="Jingle Bells Home"
     >
       <span className="flex items-baseline">
         <span
-          className={`${metalMania.className} text-gray-200 dark:text-gray-200`}
+          className={`${henny_penny.className} text-red-600 dark:text-red-500`}
         >
-          Crescent Moon
+          Crescent
+        </span>
+        <span
+          className={`${festive.className} text-green-600 dark:text-green-500 ml-2`}
+        >
+          Moon
         </span>
       </span>
     </Link>
