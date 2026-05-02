9eacc0d033b9bdd9316a52d0b60a0a6b19d0c7aa
ozsumit <pokhrelsumit36@gmail.com>
2026-05-02 21:37:02 +0545

    bb

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ async function getData() {
   const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
   const resp = await fetch(
     // `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
-    `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${apiKey}`
+    `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${apiKey}`,
   );
 
   if (!resp.ok) {
@@ -34,11 +34,11 @@ export default async function Home() {
   const data = await getData();
   return (
     <div className=" m-0 bg-[rgb(7,8,9)] h-auto">
-    {/* <Title /> */}
+      {/* <Title /> */}
       <SpotlightCarousel />
       {/* <SearchBar /> */}
       {/* <HomeFilter /> */}
-      {/* <h1>Trending Movies</h1> div*/}
+      {/* <h1>TTrending Movies</h1> div*/}
       <div className="w-full flex px-0 sm:px-4  justify-center items-center">
         <HomeDisplay movies={data} />
       </div>
